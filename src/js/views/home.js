17cb9cee11580fb8cbe0df1f7fd0fab92c11fc95
LisarWisar <60940343+LisarWisar@users.noreply.github.com>
2024-04-26 05:16:28 +0000

    testing card

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -5,12 +5,14 @@ export const Home = () => {
 	
 	return(
 		<div>
-			<div className="card" style="width: 18rem;">
+			<div className="card">
 				<img src="..." className="card-img-top" alt="..."></img>
 				<div className="card-body">
-					<h5 className="card-title">Card title</h5>
-					<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-					<a href="#" className="btn btn-primary">Go somewhere</a>
+					<h5 className="card-title">Name</h5>
+					<p className="card-text">Gender</p>
+					<p className="card-text">Hair-color</p>
+					<p className="card-text">Eye color</p>
+					<a href="#" className="btn btn-primary">Learn more!</a>
 				</div>
 			</div>
 		</div>
