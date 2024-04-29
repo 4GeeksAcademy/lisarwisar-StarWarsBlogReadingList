e4ee8f422a6c3e633d6d7955d9530aa2e535144c
LisarWisar <60940343+LisarWisar@users.noreply.github.com>
2024-04-29 23:04:45 +0000

    Horizontal scroll div for cards added

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,17 +2,73 @@ import React from "react";
 import "../../styles/home.css";
 
 export const Home = () => {
+
+	let characters = [];
+	for (let i = 0; i < 10; i++){
+		characters.push(i);
+	}
+
+	let planets = [];
+	for (let i = 0; i < 10; i++){
+		planets.push(i);
+	}
+
+	let vehicles = [];
+	for (let i = 0; i < 10; i++){
+		vehicles.push(i);
+	}
 	
 	return(
 		<div>
-			<div className="card">
-				<img src="..." className="card-img-top" alt="..."></img>
-				<div className="card-body">
-					<h5 className="card-title">Name</h5>
-					<p className="card-text">Gender</p>
-					<p className="card-text">Hair-color</p>
-					<p className="card-text">Eye color</p>
-					<a href="#" className="btn btn-primary">Learn more!</a>
+			<div className="container overflow-scroll">
+				<h2>Characters</h2>
+				<div className="row flex-nowrap">
+					{characters.map(character => (
+						<div className="card col-4 mx-3">
+							<img src="..." className="card-img-top" alt="..."></img>
+							<div className="card-body">
+								<h5 className="card-title">Name {character}</h5>
+								<p className="card-text">Gender</p>
+								<p className="card-text">Hair-color</p>
+								<p className="card-text">Eye color</p>
+								<a href="#" className="btn btn-primary">Learn more!</a>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+			<div className="container overflow-scroll">
+				<h2>Planets</h2>
+				<div className="row flex-nowrap">
+					{planets.map(planet => (
+						<div className="card col-4 mx-3">
+							<img src="..." className="card-img-top" alt="..."></img>
+							<div className="card-body">
+								<h5 className="card-title">Name {planet}</h5>
+								<p className="card-text">Gender</p>
+								<p className="card-text">Hair-color</p>
+								<p className="card-text">Eye color</p>
+								<a href="#" className="btn btn-primary">Learn more!</a>
+							</div>
+						</div>
+					))}
+				</div>
+			</div>
+			<div className="container overflow-scroll">
+				<h2>Vehicles</h2>
+				<div className="row flex-nowrap">
+					{vehicles.map(vehicle => (
+						<div className="card col-4 mx-3">
+							<img src="..." className="card-img-top" alt="..."></img>
+							<div className="card-body">
+								<h5 className="card-title">Name {vehicle}</h5>
+								<p className="card-text">Gender</p>
+								<p className="card-text">Hair-color</p>
+								<p className="card-text">Eye color</p>
+								<a href="#" className="btn btn-primary">Learn more!</a>
+							</div>
+						</div>
+					))}
 				</div>
 			</div>
 		</div>
